c7375cd6e90585c4067b5963a494eb28e27680ca
Juan Castillo <castillobgr@gmail.com>
2024-11-17 16:45:28 -0500

    async writes - WIP

--- src/generate.rs
+++ src/generate.rs
@@ -1,16 +1,14 @@
 use rand::distributions::{Alphanumeric, DistString};
 use rand::SeedableRng;
-use std::fs::File;
-use std::io::{self, Error, ErrorKind, Write};
-use std::sync::{mpsc, Arc};
+use core::str;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Error};
+use std::os::unix::fs::FileExt;
+use std::sync::Arc;
 
-use mpsc::Receiver;
 use tokio::task::JoinSet;
 
-use crate::bucket::{self, Bucket};
-
-/// The value sent by workers to the writer when they have finished processing data.
-const POISON_PILL: &str = "shutdown now";
+use crate::bucket;
 
 /// Write size_bytes random data into file, using at most max_mem (RAM).
 pub async fn generate_data(filepath: &str, size_bytes: usize, max_mem: usize) -> io::Result<()> {
@@ -27,13 +25,10 @@ pub async fn generate_data(filepath: &str, size_bytes: usize, max_mem: usize) ->
     let num_cores = std::thread::available_parallelism()?.get();
     let mem_per_core = max_mem / num_cores;
     let b = Arc::new(bucket::Bucket::new(num_cores as i32));
-    let writer_bucket = b.clone();
-    let mut set = JoinSet::new();
-    let (tx, rx) = mpsc::channel();
-
-    let writer_handle = tokio::spawn(writer(file, writer_bucket, rx, num_cores));
+    let mut set: JoinSet<io::Result<()>> = JoinSet::new();
 
     let mut remaining = size_bytes;
+    let mut offset: usize = 0;
     while remaining > 0 {
         let len = if remaining < mem_per_core {
             remaining
@@ -41,53 +36,22 @@ pub async fn generate_data(filepath: &str, size_bytes: usize, max_mem: usize) ->
             mem_per_core
         };
 
-        let tx = tx.clone();
         b.take();
+        let writer_bucket = b.clone();
+        let filepath = filepath.to_owned().clone();
         set.spawn_blocking(move || {
-            tx.send(generate(len)).map_err(|e| {
-                Error::new(
-                    io::ErrorKind::Other,
-                    format!("Could not send to channel: {e}"),
-                )
-            })
+            let f = OpenOptions::new().write(true).open(filepath)?;
+            let buf = generate(len);
+            writer_bucket.put();
+            f.write_all_at(&buf.as_bytes(), offset as u64)
         });
         remaining -= len;
+        offset += len;
     }
     while let Some(res) = set.join_next().await {
         let _ = res??;
     }
-    for _ in 0..num_cores {
-        tx.send(String::from(POISON_PILL))
-            .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
-    }
-
-    writer_handle.await?
-}
-
-async fn writer(
-    mut file: File,
-    b: Arc<Bucket>,
-    rx: Receiver<String>,
-    n_threads: usize,
-) -> io::Result<()> {
-    let mut shutdowns = 0;
-    loop {
-        if shutdowns == n_threads {
-            return Ok(());
-        }
-        let s = rx.recv().map_err(|e| {
-            Error::new(
-                io::ErrorKind::Other,
-                format!("Could not receive from channel: {e}"),
-            )
-        })?;
-        if s == POISON_PILL {
-            shutdowns += 1;
-            continue;
-        }
-        b.put();
-        file.write_all(s.as_bytes()).unwrap()
-    }
+    file.set_len(size_bytes as u64)
 }
 
 fn generate(len: usize) -> String {
